1fbf3cf3974a4249ca6d25bb438288ef2bf3b349
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-09-08 11:23:03 +0530

    react practice

---
+++ bharatclock/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import clockheading from './components/clockheading';
+
+function App() {
+  return (
+    <>
+      <div>
+      <clockheading></clockheading>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ bharatclock/src/components/clockheading.jsx
@@ -0,0 +1,4 @@
+let clockheading = () =>{
+    <h1>Bharat clock</h1>
+}
+export default clockheading;
