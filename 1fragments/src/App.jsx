5a0b582039dd1a976b0fb25a9034a0cec337f080
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-09-09 15:13:24 +0530

    REACT  PRACTICE

---
+++ 1fragments/src/App.jsx
@@ -0,0 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+ 
+
+  return (
+      <>
+        <h1>Healthy food</h1>
+        <ul class="list-group">
+  <li class="list-group-item">Dal</li>
+  <li class="list-group-item">Green Vegitable</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item"> ROTI </li>
+  <li class="list-group-item">MILK</li>
+</ul>
+</>
+       
+  )
+}
+
+export default App
--- bharatclock/src/components/clockheading.jsx
+++
@@ -1,4 +0,0 @@
-let clockheading = () =>{
-    <h1>Bharat clock</h1>
-}
-export default clockheading;
