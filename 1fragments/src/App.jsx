6d77751239644c92a4a1230181ce68c00e2141a2
Kaleemakhtarkhan <kaleemakhtarkhan1@gmail.com>
2024-09-10 11:18:03 +0530

    map() practice in react

--- 1fragments/src/App.jsx
+++ 1fragments/src/App.jsx
@@ -2,21 +2,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
- 
+ let fooditems = ["Dal", " >Green Vegitable",  "A third item", "ROTI"];
 
   return (
       <>
         <h1>Healthy food</h1>
         <ul class="list-group">
-  <li class="list-group-item">Dal</li>
+          
+  {/* <li class="list-group-item">Dal</li>
   <li class="list-group-item">Green Vegitable</li>
   <li class="list-group-item">A third item</li>
   <li class="list-group-item"> ROTI </li>
-  <li class="list-group-item">MILK</li>
+  <li class="list-group-item">fruits</li> */}
+
+  {/* or you can map the item  */}
+  {fooditems.map((item) => (
+    <li class="list-group-item">{item}</li>
+  ))}
 </ul>
 </>
        
   )
+
 }
 
 export default App
